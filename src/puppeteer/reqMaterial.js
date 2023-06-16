af0011e7b75ca36c67dc5c6971f5b6ec134799e4
Mykael Mello <eng.mykael@gmail.com>
2023-06-16 16:40:58 -0300

    ok

--- src/puppeteer/reqMaterial.js
+++ src/puppeteer/reqMaterial.js
@@ -6,7 +6,7 @@ const pupMultReqMaterial = async (codReq) => {
   try {
     if (codReq.constructor !== Array) return;
 
-    const target = 'https://sipac.ufrn.br/sipac/buscaListaReq.do';
+    // const target = 'https://sipac.ufrn.br/sipac/buscaListaReq.do';
 
     const username = process.env.USERNAMESIPAC;
     const password = process.env.PASSWORDSIPAC;
@@ -55,13 +55,15 @@ const pupMultReqMaterial = async (codReq) => {
     }); // inseri essa nova requisição apenas pq deu um bug no sipac no dia 27/10/22 que impedia o acesso direto, emitia um aviso de acesso bloqueado e era necessario fazer a requisição da url novamente para acessar. Depois que corrigirem o problema essa linha pode ser removida para permitir um retorno mais rápido dos dados.
 
     // eslint-disable-next-line max-len
-    const idReqs = await page.evaluate(async (target, codReq) => Promise.all(codReq.map(async (value) => {
+    const Reqs = await page.evaluate(async (codReq) => Promise.all(codReq.map(async (value) => {
       const numeroReq = value.split('/')[0];
       const anoReq = value.split('/')[1];
-      const searchParam = `tipoReq.id=1&buscaNumAno=true&numero=${numeroReq}&ano=${anoReq}`;
+      // const searchParam = `tipoReq.id=1&buscaNumAno=true&numero=${numeroReq}&ano=${anoReq}`;
+
+      const target = `https://sipac.ufrn.br/sipac/buscaRequisicao.do?requisicao.numero=${numeroReq}&requisicao.ano=${anoReq}&requisicao.tipo.id=1&popup=true`;
 
       const response = await fetch(target, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
         credentials: 'include', // include, *same-origin, omit
@@ -71,43 +73,7 @@ const pupMultReqMaterial = async (codReq) => {
         },
         redirect: 'follow', // manual, *follow, error
         referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: new URLSearchParams(searchParam), // body data type must match "Content-Type" header
-      });
-      const arrayBuffer = await response.arrayBuffer();
-      const decoder = new TextDecoder('iso-8859-1');
-      const textBuffer = await decoder.decode(arrayBuffer);
-
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(textBuffer, 'text/html');
-
-      // EXTRAINDO AS INFORMACOES DA REQUISICAO (PODE UNIFICAR EM UMA UNICA LINHA)
-      const tableDados = doc.querySelector('tbody.listagem');
-
-      console.dir(tableDados);
-
-      if (!tableDados) return `Requisição de material nº ${value} não localizada`;
-      if (tableDados.childNodes[1].children[6].innerText.trim() !== 'ALMOXARIFADO DE MATERIAIS DE MANUTENÇÃO DE IMÓVEIS') return `Requisição de material nº ${value} alheia a manutenção`;
-      if (!tableDados.childNodes[1].children[7].innerText.trim().include('FINALIZADA')) return `Requisição de material nº ${value} não finalizada`;
-
-      return tableDados.childNodes[1].children[11].children[0].value;
-    })), target, codReq);
-
-    const Reqs = await page.evaluate(async (idReqs) => Promise.all(idReqs.map(async (idReq) => {
-      if (!idReq) return idReq; // retorna o string de erro criado no lugar do id
-      if (!idReq.match(/^[0-9]+$/)) return idReq; // retorna o string de erro criado no lugar do id
-
-      const response = await fetch('https://sipac.ufrn.br/sipac/acompanharReqMaterial.do', {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'include', // include, *same-origin, omit
-        headers: {
-          authority: 'sipac.ufrn.br',
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: new URLSearchParams(`id=${idReq}&acao=200`), // body data type must match "Content-Type" header
+        // body: new URLSearchParams(searchParam), // body data type must match "Content-Type" header
       });
       const arrayBuffer = await response.arrayBuffer();
       const decoder = new TextDecoder('iso-8859-1');
@@ -148,7 +114,7 @@ const pupMultReqMaterial = async (codReq) => {
       }
 
       return { dadosJSON, itensJSON };
-    })), idReqs);
+    })), codReq);
 
     await browser.close();
 
--- src/puppeteer/reqMultMaterial.js
+++ src/puppeteer/reqMultMaterial.js
@@ -6,8 +6,6 @@ const pupMultReqMaterial = async (codReq) => {
   try {
     if (codReq.constructor !== Array) return;
 
-    const target = 'https://sipac.ufrn.br/sipac/buscaListaReq.do';
-
     const username = process.env.USERNAMESIPAC;
     const password = process.env.PASSWORDSIPAC;
 
@@ -50,18 +48,20 @@ const pupMultReqMaterial = async (codReq) => {
 
     );
 
-    await page.goto('https://sipac.ufrn.br/sipac/portal_administrativo/index.jsf', {
-      waitUntil: 'networkidle2',
-    }); // inseri essa nova requisição apenas pq deu um bug no sipac no dia 27/10/22 que impedia o acesso direto, emitia um aviso de acesso bloqueado e era necessario fazer a requisição da url novamente para acessar. Depois que corrigirem o problema essa linha pode ser removida para permitir um retorno mais rápido dos dados.
+    // await page.goto('https://sipac.ufrn.br/sipac/portal_administrativo/index.jsf', {
+    //   waitUntil: 'networkidle2',
+    // });
+    // inseri essa nova requisição apenas pq deu um bug no sipac no dia 27/10/22 que impedia o acesso direto, emitia um aviso de acesso bloqueado e era necessario fazer a requisição da url novamente para acessar. Depois que corrigirem o problema essa linha pode ser removida para permitir um retorno mais rápido dos dados.
 
     // eslint-disable-next-line max-len
-    const idReqs = await page.evaluate(async (target, codReq) => Promise.all(codReq.map(async (value) => {
+    const Reqs = await page.evaluate(async (codReq) => Promise.all(codReq.map(async (value) => {
       const numeroReq = value.split('/')[0];
       const anoReq = value.split('/')[1];
-      const searchParam = `tipoReq.id=1&buscaNumAno=true&numero=${numeroReq}&ano=${anoReq}`;
+
+      const target = `https://sipac.ufrn.br/sipac/buscaRequisicao.do?requisicao.numero=${numeroReq}&requisicao.ano=${anoReq}&requisicao.tipo.id=1&popup=true`;
 
       const response = await fetch(target, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
         credentials: 'include', // include, *same-origin, omit
@@ -71,7 +71,7 @@ const pupMultReqMaterial = async (codReq) => {
         },
         redirect: 'follow', // manual, *follow, error
         referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: new URLSearchParams(searchParam), // body data type must match "Content-Type" header
+        // body: new URLSearchParams(searchParam), // body data type must match "Content-Type" header
       });
       const arrayBuffer = await response.arrayBuffer();
       const decoder = new TextDecoder('iso-8859-1');
@@ -80,45 +80,20 @@ const pupMultReqMaterial = async (codReq) => {
       const parser = new DOMParser();
       const doc = parser.parseFromString(textBuffer, 'text/html');
 
-      // EXTRAINDO AS INFORMACOES DA REQUISICAO (PODE UNIFICAR EM UMA UNICA LINHA)
-      const tableDados = doc.querySelector('tbody.listagem');
+      // VERIFICAR SE A REQUISIÇÃO É VÁLIDA
+      const isInvalid = doc.querySelector('#container-popup > table > tbody > tr:nth-child(1) > td.erro');
 
-      console.dir(tableDados);
-
-      if (!tableDados) return `Requisição de material nº ${value} não localizada`;
-      if (tableDados.childNodes[1].children[6].innerText.trim() !== 'ALMOXARIFADO DE MATERIAIS DE MANUTENÇÃO DE IMÓVEIS') return `Requisição de material nº ${value} alheia a manutenção`;
-      if (!tableDados.childNodes[1].children[7].innerText.trim().include('FINALIZADA')) return `Requisição de material nº ${value} não finalizada`;
-
-      return tableDados.childNodes[1].children[11].children[0].value;
-    })), target, codReq);
-
-    const Reqs = await page.evaluate(async (idReqs) => Promise.all(idReqs.map(async (idReq) => {
-      if (!idReq) return idReq; // retorna o string de erro criado no lugar do id
-      if (!idReq.match(/^[0-9]+$/)) return idReq; // retorna o string de erro criado no lugar do id
-
-      const response = await fetch('https://sipac.ufrn.br/sipac/acompanharReqMaterial.do', {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'include', // include, *same-origin, omit
-        headers: {
-          authority: 'sipac.ufrn.br',
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: new URLSearchParams(`id=${idReq}&acao=200`), // body data type must match "Content-Type" header
-      });
-      const arrayBuffer = await response.arrayBuffer();
-      const decoder = new TextDecoder('iso-8859-1');
-      const textBuffer = await decoder.decode(arrayBuffer);
-
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(textBuffer, 'text/html');
+      if (isInvalid) return `Requisição de material nº ${value} não localizada`;
 
       // EXTRAINDO AS INFORMACOES DA REQUISICAO (PODE UNIFICAR EM UMA UNICA LINHA)
       const tableDados = Array.from(doc.querySelector('table.formulario tbody').children);
+
       const dados = tableDados.map((e) => e.innerText.replace(/[\n\t\r]/g, '').trim().split(':'));
+
+      // VERIFICAR SE É RELACIONADA A MANUTENÇÃO
+      console.log(dados[13][1]);
+      if (!dados[13][1].includes('MANUTENÇÃO')) return `Requisição de material nº ${value} alheia a manutenção`;
+
       dados.length = 17; // REMOVER LINHAS DESNECESSARIAS
 
       // TABLE TO JSON (dados)
@@ -130,6 +105,7 @@ const pupMultReqMaterial = async (codReq) => {
 
       // EXTRAINDO AS INFORMACOES DOS TITULOS DOS ITENS (PODE UNIFICAR EM UMA UNICA LINHA)
       const tableCampos = Array.from(doc.querySelector('table.formulario table thead tr').children);
+
       const campos = tableCampos.map((e) => e.innerText.replace(/[\n\t\r]/g, '').trim());
 
       // EXTRAINDO AS INFORMACOES DOS ITENS (PODE UNIFICAR EM UMA UNICA LINHA)
@@ -147,8 +123,11 @@ const pupMultReqMaterial = async (codReq) => {
         itensJSON.push(itemJSON);
       }
 
+      // VERIFICAR SE O STATUS É FINALIZADA
+      if (!dadosJSON['Status Atual'].includes('FINALIZADA')) return `Requisição de material nº ${value} não finalizada`;
+
       return { dadosJSON, itensJSON };
-    })), idReqs);
+    })), codReq);
 
     await browser.close();
 
